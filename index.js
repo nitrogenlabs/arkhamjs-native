067d077fb2d3227d9bf9ea75027dba45c609f842
Giraldo Rosales <nitrog7@gmail.com>
2017-12-12 00:01:39 -0600

    Move compiled to lib

--- index.js
+++ index.js
@@ -2,4 +2,19 @@
  * Copyright (c) 2017, Nitrogen Labs, Inc.
  * Copyrights licensed under the MIT License. See the accompanying LICENSE file for terms.
  */
-module.exports = require('./dist/index');
+
+// Framework
+export { Flux, FluxAction, FluxOptions, FluxDebugLevel } from './lib/Flux/Flux';
+
+// Store
+export { Store } from './lib/Store/Store';
+
+// Actions
+export { ArkhamActions } from './lib/actions/ArkhamActions/ArkhamActions';
+
+// Constants
+export { ArkhamConstants } from './lib/constants/ArkhamConstants';
+
+// Components
+export { Arkham } from './lib/components/Arkham/Arkham';
+export { NavigationParams, MatchProps, View, ViewProps } from './lib/components/View/View';
